e8e2ffae8530f56306b5660def943b2267c84549
Rudy Gevaert <rudy.gevaert@ugent.be>
2011-12-27 22:58:54 +0100

    Manage the mirror.list file

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,48 @@
 # Class: debianmirror
 #
-# This module manages debianmirror
+# This module manages debian mirrors.
 #
 # Parameters:
+# template 
+# mirror
+# distribution
+# components
 #
 # Actions:
 #
-# Requires:
+# Requires: apt-mirror debian package
 #
 # Sample Usage:
 #
+#   include debianmirror
+#   debianmirror::instance{
+#     test:
+#   }
+#
 # [Remember: No empty lines between comments and class definition]
-class debianmirror {
+class debianmirror (
+    $template = 'debianmirror/mirror.list.erb',
+    $mirror = 'ftp.us.debian.org',
+    $distribution = 'unstable',
+    $components = 'main contrib non-free') {
 
+  package {
+    'apt-mirror':
+      ensure => present;
+  }
 
+  file {
+    '/etc/apt/mirror.list':
+        ensure => present,
+        mode    => 644,
+        owner   => root,
+        group   => root,
+        content => template($template);
+    '/etc/cron.d/apt-mirror':
+        ensure  => present,
+        mode    => 644,
+        owner   => root,
+        group   => root,
+        content => template('debianmirror/cron.erb');
+  }
 }
